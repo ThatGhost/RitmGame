85329b6d478e5115b211d247f8e355f83dac02e9
PeakAlive <sasha.vigneron@icloud.com>
2020-11-24 15:00:11 +0100

    Updated

--- Game_08_IbnZwanckaert_Nathan_SashaVigneron_NathanVerschuere/MainGame.cpp
+++ Game_08_IbnZwanckaert_Nathan_SashaVigneron_NathanVerschuere/MainGame.cpp
@@ -12,7 +12,42 @@ void MainGame::Start() {
 
 void MainGame::Draw() {
 	DrawTexture(*GetTexture("Background.png"),Point2f(0,0));
+
+	SetColor(0.3f, 0.2f, 0.3f); // Purple Color
+	Point2f rectPosition{ 60, 25 };
+
+
+
+	float duckSize{ 250 };
+	Point2f duckPosition{ 10, -50 };
+	Rectf duckRect{ duckPosition.x, duckPosition.y, duckSize, duckSize }; // x, y, scaleX, scaleY
+
+	//Change later <- edges are shit
+	DrawRect(rectPosition.x, rectPosition.y, 1280 - (2 * rectPosition.x), 100, 8);
+	DrawRect(rectPosition.x + 200, rectPosition.y, 100, 100, 8);
+
+	DrawTexture(*GetTexture("Duck1.png"), duckRect);
+
+	DrawDucks(g_DuckArray);
+
 }
+
+void MainGame::SpawnDuck(const Point2f& spawnPosition)
+{
+
+}
+
+void MainGame::DrawDucks(const int array[])
+{
+	float duckSize{ 100 };
+
+	for (int i = 0; i < duckArraySize; i++)
+	{
+		Rectf duckRect{ float(array[i]), 25.0f, duckSize, duckSize };
+		DrawTexture(*GetTexture("Duck2.png"), duckRect);
+	}
+}
+
 
 void MainGame::End() {
 
--- Game_08_IbnZwanckaert_Nathan_SashaVigneron_NathanVerschuere/MainGame.h
+++ Game_08_IbnZwanckaert_Nathan_SashaVigneron_NathanVerschuere/MainGame.h
@@ -1,12 +1,24 @@
 #pragma once
+const int duckArraySize{ 3 };
+
 class MainGame
 {
+
 public:
 	void Draw();
 	void Update(float elapsedSec);
 	void Start();
 	void End();
+
+	void SpawnDuck(const Point2f& spawnPosition);
 private:
+
+
+	Point2f g_SpawnPoint{ 200,10 };
+
+	int g_DuckArray[duckArraySize]{ (1280 - 150) };
+
+	void DrawDucks(const int array[]);
 };
 
 class MainMenu
